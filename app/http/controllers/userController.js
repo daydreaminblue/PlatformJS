b26349125a897ac60e430ab340f171c0d57a09fc
daydreaminblue <roozbehabbasirad2@gmail.com>
2020-12-11 18:28:06 +0330

    adding user panel

---
+++ app/http/controllers/userController.js
@@ -0,0 +1,15 @@
+const controller = require('app/http/controllers/controller');
+
+class userController extends controller {
+    
+    async index(req , res , next) {
+        try {
+            res.render('home/panel/index' , { title : 'پنل کاربری'});
+        } catch (err) {
+            next(err)
+        }
+    }
+
+}
+
+module.exports = new userController();
